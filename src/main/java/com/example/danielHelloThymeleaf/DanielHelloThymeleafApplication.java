c0cb67922195cb071d7b1a87a827d172b4e8f665
DANIEL CHAUVIERE <bgq007@myy.haaga-helia.fi>
2022-09-05 17:21:00 +0300

    first commit

---
+++ src/main/java/com/example/danielHelloThymeleaf/DanielHelloThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.example.danielHelloThymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanielHelloThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanielHelloThymeleafApplication.class, args);
+	}
+
+}
